fd89a54a25cda43807eb195dbc1a121ae34e0b1c
Flemming Grabowski <131593478+FlemmoG@users.noreply.github.com>
2026-06-21 10:06:48 -0700

    Add FinancialAdvisor JPA entity

---
+++ entities/FinancialAdvisor.java
@@ -0,0 +1,71 @@
+package entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class FinancialAdvisor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @OneToMany(mappedBy = "financialAdvisor")
+    private List<Client> clients;
+
+    public FinancialAdvisor() {
+    }
+
+    public FinancialAdvisor(String firstName, String lastName, String email, List<Client> clients) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.clients = clients;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
+    }
+}
